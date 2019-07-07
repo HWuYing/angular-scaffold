3532887d7e9c91e03c542bcc3d528d8e203d1c0b
237617561@qq.com <*n19900814x*>
2019-07-07 17:50:19 +0800

    ng-zorro-lazy

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -4,7 +4,6 @@ import zh from '@angular/common/locales/zh';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { BaseComponentsModule } from '../common/components/base-components.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './container/app.component';
 
